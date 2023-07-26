6a5d97bfd5ece4151b630a6264bd4ea34b52ca5c
Deborah Wyny <129215842+deborahwyny@users.noreply.github.com>
2023-07-26 19:31:54 -0300

    modificação schemma cliente

--- src/schemmas/clientes.schemma.js
+++ src/schemmas/clientes.schemma.js
@@ -2,7 +2,7 @@ import Joi from 'joi'
 
 export const schemmaClientes = Joi.object({
   name: Joi.string().required(),
-  phone: Joi.string().min(10).max(11).required(),
-  cpf: Joi.string().length(11).required(),
+  phone: Joi.string().min(10).max(11).pattern(/^[0-9]{11}$/).required(),
+  cpf: Joi.string().length(11).pattern(/^[0-9]{11}$/).required(),
   birthday: Joi.date().iso().required()
 })
